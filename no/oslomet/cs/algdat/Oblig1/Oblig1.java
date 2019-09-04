ddac76bab490a7472d338bacfd7bf92f465468e7
Jovalb <jovorapper1@gmail.com>
2019-09-04 17:06:12 +0200

    Lagt inn liten test utskrift i oblig1 java

--- no/oslomet/cs/algdat/Oblig1/Oblig1.java
+++ no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -11,7 +11,9 @@ import java.util.NoSuchElementException;
 
 public class Oblig1 {
 
-
+    public static void main(String[] args) {
+        System.out.println("Dette er en test");
+    }
 
     private Oblig1() {
     }
